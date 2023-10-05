7e9329ded2ffbe31ba2c5d94b4d2e71de0fa6a23
Samuel25100 <samuelgselassie1@gmail.com>
2023-10-05 12:09:21 +0300

    add file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,10 +5,6 @@
  */
 int main(void)
 {
-char array[] = "and that piece of art is useful\"";
-char array2[] = " - Dora Korpar, 2015-10-19\n";
-
-	write(2, array, sizeof(array));
-	write(2, array2, sizeof(array2));
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
