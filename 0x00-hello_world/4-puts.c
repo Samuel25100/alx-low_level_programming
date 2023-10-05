098efb6806d4683bd295020547d2736105db58c7
Samuel25100 <samuelgselassie1@gmail.com>
2023-10-05 11:17:35 +0300

    add file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - print input string
+ * Return: 0
+ */
+int main(void)
+{
+	char array[] = "\"Programming is like building a multilingual puzzle";
+
+		puts(array);
+		return (0);
+}
