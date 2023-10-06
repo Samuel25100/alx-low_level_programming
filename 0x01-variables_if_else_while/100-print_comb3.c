379714390d1a828c06102ba36045f890e0ee415d
Samuel25100 <samuelgselassie1@gmail.com>
2023-10-06 19:09:05 +0300

    add file

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - print all commbination of 2 digit numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int x, y;
+
+	for (x = 48; x <= 56; x++)
+	{
+	for (y = 48; y <= 57; y++)
+	{
+	putchar(x);
+	if (x != 56)
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
