0c9a208c2f4c1c08e2260634d273d4ded7df9f0c
Samuel25100 <samuelgselassie1@gmail.com>
2023-10-06 18:21:31 +0300

    add file

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print alphabet in lowercase
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 97; x <= 122; x++)
+	{
+	putchar(x);
+	}
+	putchar("\n");
+	return (0);
+}
