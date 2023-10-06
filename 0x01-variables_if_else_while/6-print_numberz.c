de29d4f851b88ec1100a4baf19f21cdb834b933c
Samuel25100 <samuelgselassie1@gmail.com>
2023-10-06 18:38:59 +0300

    add file

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print number from 0 - 9
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 48; x <= 57; x++)
+	{
+	putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
