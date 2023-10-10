a42ab413a9bcbd9d49b9f7189696181762076c81
Samuel25100 <samuelgselassie1@gmail.com>
2023-10-10 09:08:55 +0300

    add file

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _islower - check for lower case letter return 1 for lower c
+ * @c: the char
+ * Return: 0
+ */
+int _islower(int c)
+{
+	if (c <= 122 || c >= 97)
+	{
+	return (1);
+	}
+	return (0);
+}
