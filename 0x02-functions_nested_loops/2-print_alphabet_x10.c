0b27f6c9fcdd092172522f20620773d74d8f04a0
Samuel25100 <samuelgselassie1@gmail.com>
2023-10-10 09:01:06 +0300

    add file

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print a-z 10x time
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int x, y;
+
+	for (x = 0; x <= 10; x++)
+	{
+	for (y = 97; y <= 122; y++)
+	{
+		_putchar(x);
+	}
+	}
+	_putchar("\n");
+}
