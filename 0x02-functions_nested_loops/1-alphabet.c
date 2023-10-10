541ac5c4b951bc0cdcd8c3ae1945c03f9a73d8f9
Samuel25100 <samuelgselassie1@gmail.com>
2023-10-10 08:52:07 +0300

    add file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - print a-z
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	int x = 97;
+
+	for(; x <= 122; x++)
+	{
+	_putchar(x);
+	}
+	_putchar("\n");
+}
