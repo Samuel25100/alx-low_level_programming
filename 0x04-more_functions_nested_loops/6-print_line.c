7836c976e96549db9db2bda494adf2246f8241bc
Samuel25100 <samuelgselassie1@gmail.com>
2023-10-12 09:58:44 +0300

    add file

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_line - print n number of "_"
+ * @n: num
+ * Return: void
+ */
+void print_line(int n)
+{
+	int x;
+
+	for (x = 1; x <= n; x++)
+	{
+	_putchar(95);
+	}
+	_putchar('\n');
+}
