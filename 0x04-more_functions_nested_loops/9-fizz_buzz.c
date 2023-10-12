82bc14acadf4c6b2f901210e67515f8e16b87861
Samuel25100 <samuelgselassie1@gmail.com>
2023-10-12 14:15:19 +0300

    add file

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,18 +11,22 @@ int main(void)
 	{
 	if (!(x % 5) && !(x % 3))
 	{
-	printf("FizzBuzz ");
+	printf(" FizzBuzz");
 	}
 	else if (!(x % 5))
 	{
-	printf("Buzz ");
+	printf(" Buzz");
 	}
 	else if (!(x % 3))
 	{
-	printf("Fizz ");
+	printf(" Fizz");
+	}
+	else if (x == 1)
+	{
+	printf("%d", x);
 	}
 	else
-	printf("%d ",x);
+	printf(" %d", x);
 	}
 	putchar('\n');
 	return (0);
