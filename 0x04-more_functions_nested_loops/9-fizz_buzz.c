6496e38a983644696954ce64e6302362a9ee488d
Samuel25100 <samuelgselassie1@gmail.com>
2023-10-12 14:03:02 +0300

    add file

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - print form 1 - 100 but multiple of 3 "Fizz" 5x Buzz both FB
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 1; x <= 100; x++)
+	{
+	if (!(x % 5) && !(x % 3))
+	{
+	printf("FizzBuzz ");
+	}
+	else if (!(x % 5))
+	{
+	printf("Buzz ");
+	}
+	else if (!(x % 3))
+	{
+	printf("Fizz ");
+	}
+	else
+	printf("%d ",x);
+	}
+	putchar('\n');
+	return (0);
+}
