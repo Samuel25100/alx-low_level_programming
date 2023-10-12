e1f2faac678b81b171b474622d6e4ebd37957cee
Samuel25100 <samuelgselassie1@gmail.com>
2023-10-12 15:48:18 +0300

    add file

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * print_triangle - print tringle in size
+ * @size: the size of the triangle
+ * Return: void
+ */
+void print_triangle(int size)
+{
+	int x, y, z;
+
+	if (size == 0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
+	for (x = 1; x <= size; x++)
+	{
+	z = 32;
+	for (y = size; y >= 1; y--)
+	{
+	if (y == x)
+	{
+	z = 35;
+	}
+	_putchar(z);
+	}
+	_putchar('\n');
+	}
+	}
+}
