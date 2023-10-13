7220e96e7310114ef714909ebf9385f036d72a09
Samuel25100 <samuelgselassie1@gmail.com>
2023-10-13 11:08:41 +0300

    aad file

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,11 +8,11 @@ void print_number(int n)
 {
 	if (n < 0)
 	{
-	putchar('-');
+	_putchar('-');
 	}
 	if (n / 10)
 	{
 	print_number(n / 10);
 	}
-	putchar((n % 10) + '0');
+	_putchar((n % 10) + '0');
 }
