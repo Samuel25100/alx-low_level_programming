4a3f5de63f0ff4519c390bd4084201d625997c8e
Samuel25100 <samuelgselassie1@gmail.com>
2023-10-16 12:58:10 +0300

    add file

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * _strlen - return the lenght og the string
+ * @s: the string
+ * Return: lenght
+ */
+int _strlen(char *s)
+{
+	int x;
+
+	x = 0;
+	while (*(s + x))
+	{
+	x++;
+	}
+	return (x);
+}
+/**
+ * puts_half - print half of the string
+ * @str: the string
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int x, y;
+
+	x = (_strlen(str)) / 2;
+	for (y = 0; *(str + y); y++)
+	{
+	if (y >= x)
+	{
+	_putchar(*(str + y));
+	}
+	}
+	_putchar('\n');
+}
