ed59a0ab06145d74bca9d5971618e06481060257
Samuel25100 <samuelgselassie1@gmail.com>
2023-10-16 13:59:43 +0300

    add file 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strcpy - copy the string from src to dest
+ * @src: source of string
+ * @dest: destination of string
+ * Return: pointer of dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int x;
+
+	for (x = 0; *(src + x); x++)
+	{
+	*(dest + x) = *(src + x);
+	}
+	*(dest + (x + 1)) = '\0';
+	return (dest);
+}
