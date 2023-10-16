536e2ff07c5e4f7e5fbfcf58004d2f8d570101ff
Samuel25100 <samuelgselassie1@gmail.com>
2023-10-16 16:49:12 +0300

    add file

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * _atoi - convert a string to an integer
+ * @s: the string
+ * Return: the int
+ */
+int _atoi(char *s)
+{
+	int x, y = 0, sign = 1;
+
+	for (x = 0; *(s + x); x++)
+	{
+		if (*(s + x) == '-')
+		{
+			if (*(s + x + 1) >= 48 && *(s + x + 1) <= 57)
+			{
+			if (*(s + x) == '-')
+			sign = sign * -1;
+			}
+		}
+		if (*(s + x) >= 48 && *(s + x) <= 57)
+		{
+			y = (y * 10) + (*(s + x) - '0');
+		if (!((*(s + x + 1) >= 48) && (*(s + x + 1) <= 57)))
+			{
+			break;
+			}
+		}
+	}
+	y = y * sign;
+	return (y);
+}
