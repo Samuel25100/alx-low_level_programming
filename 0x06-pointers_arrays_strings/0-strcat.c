a51a31364d9e578d2a5b70d8960535801da8b84a
Samuel25100 <samuelgselassie1@gmail.com>
2023-10-18 09:12:58 +0300

    add file

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strcat - concatenates src on dest string
+ * @dest: the destination string
+ * @src: the source string
+ * Return: pointer to dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int x, y;
+
+	x = 0;
+	while (*(dest + x))
+	{
+	x++;
+	}
+	for (y = 0; *(src + y); y++)
+	{
+	*(dest + x) = *(src + y);
+	x++;
+	}
+	*(dest + x) = '\0';
+	return (dest);
+}
