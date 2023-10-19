8c600aa4de0f6c7f927e466be364ee4ed5722218
Samuel25100 <samuelgselassie1@gmail.com>
2023-10-19 07:38:08 +0300

    add file

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * rot13 - encodes a string using rot13
+ * @str: the string
+ * Return: pointer to str
+ */
+char *rot13(char *str)
+{
+char ar1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char ar2[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int x, y;
+	
+	for (x = 0; *(str + x); x++)
+	{
+	for (y = 0; y < 52; y++)
+	{
+	if (*(str + x) == ar1[y])
+	{
+	*(str + x) = ar2[y];
+	break;
+	}
+	}
+	}
+	return (str);
+}
