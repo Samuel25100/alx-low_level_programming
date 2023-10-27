bfca1dc833816dced8f700168446b1eb3a263b05
Samuel25100 <samuelgselassie1@gmail.com>
2023-10-27 11:33:48 +0300

    add file

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+int _atoi(char *s);
+/**
+ * main - print result of multiplication 
+ * @argc: the number of argument
+ * @argv: the array of each argument
+ * Return: 0 success
+ */
+int main(int argc, char *argv[])
+{
+	int x, y;
+
+	if (argc < 2)
+	{
+	printf("Error\n");
+	return (1);
+	}
+	x = _atoi(argv[1]);
+	y = _atoi(argv[2]);
+	printf("%d\n", x * y);
+	return (0);
+}
+/**
+ * _atoi - change char value to int
+ * @s: the string
+ * Return: the int
+ */
+int _atoi(char *s)
+{
+	int x, sign = 1;
+	unsigned int y = 0;
+
+	for (x = 0; *(s + x); x++)
+	{
+		if (*(s + x) == '-')
+		{
+		sign = sign * -1;
+		}
+		if (*(s + x) >= 48 && *(s + x) <= 57)
+		{
+		y = (y * 10) + (*(s + x) - '0');
+		if (!((*(s + x + 1) >= 48) && (*(s + x + 1) <= 57)))
+		{
+			break;
+			}
+		}
+	}
+	y = y * sign;
+	return (y);
+}
