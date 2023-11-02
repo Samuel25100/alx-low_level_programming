411c635d512914a3931ebd8b0d6dd29d60728e3e
Samuel25100 <samuelgselassie1@gmail.com>
2023-11-02 14:35:18 +0300

    add file

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -21,6 +21,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 	len2++;
 	}
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	if (n < len2)
 		tot = len1 + n + 1;
 	else
