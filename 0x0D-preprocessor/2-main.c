ca4206a299c1b0aa0f884ac51662a8e19b6e4739
Samuel25100 <samuelgselassie1@gmail.com>
2023-11-06 09:16:38 +0300

    add file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print the name of the file it was compiled from
+ * Return: 0 success
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
