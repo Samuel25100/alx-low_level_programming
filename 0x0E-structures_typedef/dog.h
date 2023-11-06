c72e0e103a56ec1ff17e667f404629577281bb35
Samuel25100 <samuelgselassie1@gmail.com>
2023-11-06 12:58:27 +0300

    add file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,6 +6,7 @@
 		float age;
 		char *owner;
 		};
-void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+typedef struct dog dog_t;
 #endif
