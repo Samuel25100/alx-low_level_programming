7d12d2f85ca391d6a71995bbb52372c19825061a
Samuel25100 <samuelgselassie1@gmail.com>
2023-11-16 15:19:01 +0300

    add string.h in lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -15,6 +15,7 @@ typedef struct list_s
 } list_t;
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 size_t print_list(const list_t *h);
