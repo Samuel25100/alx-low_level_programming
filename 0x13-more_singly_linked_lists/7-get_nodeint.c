84af0dfd1cbe24f7c41916fb28f41ef0e4010d8f
Samuel25100 <samuelgselassie1@gmail.com>
2023-11-20 17:37:34 +0300

    add file 7-get_nodeint.c

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * get_nodeint_at_index - returns the nth node of a listint_t linked list
+ * @head: pointer to the head of node
+ * @index: the index of the nodes in liked list
+ * Return: the data at index
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int count;
+
+	while (head != NULL)
+	{
+	if (count == index)
+	{
+	break;
+	}
+	head = head->next;
+	count++;
+	}
+	return (head);
+}
