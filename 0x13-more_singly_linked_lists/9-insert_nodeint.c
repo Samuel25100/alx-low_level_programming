71610775b2a4fdfc7784473c6db320d408bb3bb9
Samuel25100 <samuelgselassie1@gmail.com>
2023-11-21 13:22:40 +0300

    add file 9-insert_nodeint

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -30,7 +30,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		count++;
 		}
 	}
-	if (count < indx)
+	if (count < idx)
 		return (NULL);
 	if ((befoindx == NULL) && (idx != 0))
 	{
