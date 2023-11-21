21cd03bb36bb802d8fa0edc326808e00022a11b7
Samuel25100 <samuelgselassie1@gmail.com>
2023-11-21 14:23:26 +0300

    fixing file 7-get_node..

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,11 +12,11 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	while (head != NULL)
 	{
 	if (count == index)
-		break;
+	{
+	break;
+	}
 	head = head->next;
 	count++;
 	}
-	if (count < index)
-		return (NULL);
 	return (head);
 }
