d160619017d75774bec3bbfd22a6f727b1886043
Samuel25100 <samuelgselassie1@gmail.com>
2023-11-21 14:19:26 +0300

    add file 10-delete_nodeint

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -9,9 +9,10 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	unsigned int i;
 	listint_t *prev;
-	listint_t *nex;
+	listint_t *next;
 
 	prev = *head;
+
 	if (index != 0)
 	{
 		for (i = 0; i < index - 1 && prev != NULL; i++)
@@ -19,20 +20,24 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 			prev = prev->next;
 		}
 	}
+
 	if (prev == NULL || (prev->next == NULL && index != 0))
 	{
 		return (-1);
 	}
-	nex = prev->next;
+
+	next = prev->next;
+
 	if (index != 0)
 	{
-		prev->next = nex->next;
+		prev->next = next->next;
 		free(next);
 	}
 	else
 	{
 		free(prev);
-		*head = nex;
+		*head = next;
 	}
+
 	return (1);
 }
