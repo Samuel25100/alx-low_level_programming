4a9f2db67ef8c635d717901494cdb2a4d6ff09ef
Samuel25100 <samuelgselassie1@gmail.com>
2023-11-21 13:11:45 +0300

    fixing file 9-insert_nodeint

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -8,25 +8,39 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *temp, *temp1, *new_node;
+	listint_t *atindx, *befoindx, *new_node;
 	unsigned int count = 0;
 
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
 
-	temp = *head;
-	while (temp != NULL)
-	{
-	if (count == (idx - 1))
-		temp1 = temp;
-	if (count == idx)
-		break;
-	temp = temp->next;
-	count++;
-	}
-	new_node->next = temp;
 	new_node->n = n;
-	temp1->next = new_node;
+	atindx = *head;
+
+	if (idx != 0)
+	{
+		while (atindx != NULL)
+		{
+		if (count == (idx - 1))
+			befoindx = atindx;
+		if (count == idx)
+			break;
+		atindx = atindx->next;
+		count++;
+		}
+	}
+	if (befoindx == NULL && idx != 0)
+			return (NULL);
+	if (idx == 0)
+	{
+	new_node->next = *head;
+	*head = new_node;
+	}
+	else
+	{
+	new_node->next = atindx;
+	befoindx->next = new_node;
+	}
 	return (new_node);
 }
