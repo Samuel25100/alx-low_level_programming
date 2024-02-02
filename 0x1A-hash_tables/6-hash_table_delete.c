db4f740dae24b4facdcf62c777f7747ffb0b57cd
Samuel25100 <samuelgselassie1@gmail.com>
2024-02-02 21:36:33 +0300

    add 6-hash_table_delete

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+/**
+ * hash_table_delete - delete all memory held by hash table
+ * @ht: the pointer to hash
+ * Return: void
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i = 0;
+	hash_node_t *node, *tmp;
+
+	if (ht == NULL)
+		return;
+	for(i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node != NULL)
+		{
+			tmp = node;
+			node = node->next;
+			free(tmp->key);
+			free(tmp->value);
+			free(tmp);
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
