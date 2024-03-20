9282e6f594891f768837e12d73cc0ec4d67c2d3e
Samuel25100 <samuelgselassie1@gmail.com>
2024-03-20 19:08:57 +0300

    add file 5

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Contain method that calculate perimeter."""
+
+
+def island_perimeter(grid):
+    """Return the perimeter of the island described in grid."""
+    inn = 0
+    peri = 0
+
+    for x in range(len(grid)):
+        for y in range(len(grid[0])):
+            if grid[x][y] == 1:
+                peri += 4
+                if y != (len(grid[0]) - 1) and (x != (len(grid)) - 1):
+                    if grid[x + 1][y] == 1 or grid[x][y + 1] == 1:
+                        inn += 1
+                if grid[x - 1][y] == 1 or grid[x][y - 1] == 1:
+                    inn += 1
+    return (peri - inn)
